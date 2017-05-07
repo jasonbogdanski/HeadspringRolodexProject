75f798751684d7df7fd7aef60797e151704288d5
Jason Bogdanski <jason.bogdanski@headspring.com>
2017-05-07 17:31:09 -0500

    Updated schema scripts to use generic Id name.

--- src/Database/DatabaseMigration/up/201704010043_create_initial_database_schema.sql
+++ src/Database/DatabaseMigration/up/201704010043_create_initial_database_schema.sql
@@ -1,26 +1,26 @@
 CREATE TABLE [dbo].[BranchLocation] (
-    [BranchLocationId] [int] NOT NULL IDENTITY,
+    [Id] [int] NOT NULL IDENTITY,
     [City] [nvarchar](20),
     [State] [nvarchar](20),
-    CONSTRAINT [PK_dbo.BranchLocation] PRIMARY KEY ([BranchLocationId])
+    CONSTRAINT [PK_dbo.BranchLocation] PRIMARY KEY ([Id])
 )
 CREATE TABLE [dbo].[Employee] (
-    [EmployeeId] [int] NOT NULL IDENTITY,
+    [Id] [int] NOT NULL IDENTITY,
     [FistName] [nvarchar](20),
     [LastName] [nvarchar](30),
     [JobTitle] [nvarchar](50),
     [Email] [nvarchar](50),
     [BranchLocation_BranchLocationId] [int],
-    CONSTRAINT [PK_dbo.Employee] PRIMARY KEY ([EmployeeId])
+    CONSTRAINT [PK_dbo.Employee] PRIMARY KEY ([Id])
 )
-CREATE INDEX [IX_BranchLocation_BranchLocationId] ON [dbo].[Employee]([BranchLocation_BranchLocationId])
+CREATE INDEX [IX_BranchLocation_Id] ON [dbo].[Employee]([BranchLocation_Id])
 CREATE TABLE [dbo].[PhoneNumber] (
-    [PhoneNumberId] [int] NOT NULL IDENTITY,
+    [Id] [int] NOT NULL IDENTITY,
     [PhoneType] [int] NOT NULL,
     [Number] [nvarchar](20),
     [EmployeeModel_EmployeeId] [int],
-    CONSTRAINT [PK_dbo.PhoneNumber] PRIMARY KEY ([PhoneNumberId])
+    CONSTRAINT [PK_dbo.PhoneNumber] PRIMARY KEY ([Id])
 )
-CREATE INDEX [IX_EmployeeModel_EmployeeId] ON [dbo].[PhoneNumber]([EmployeeModel_EmployeeId])
-ALTER TABLE [dbo].[Employee] ADD CONSTRAINT [FK_dbo.Employee_dbo.BranchLocation_BranchLocation_BranchLocationId] FOREIGN KEY ([BranchLocation_BranchLocationId]) REFERENCES [dbo].[BranchLocation] ([BranchLocationId])
-ALTER TABLE [dbo].[PhoneNumber] ADD CONSTRAINT [FK_dbo.PhoneNumber_dbo.Employee_EmployeeModel_EmployeeId] FOREIGN KEY ([EmployeeModel_EmployeeId]) REFERENCES [dbo].[Employee] ([EmployeeId])
+CREATE INDEX [IX_EmployeeModel_Id] ON [dbo].[PhoneNumber]([EmployeeModel_Id])
+ALTER TABLE [dbo].[Employee] ADD CONSTRAINT [FK_dbo.Employee_dbo.BranchLocation_BranchLocation_Id] FOREIGN KEY ([BranchLocation_Id]) REFERENCES [dbo].[BranchLocation] ([Id])
+ALTER TABLE [dbo].[PhoneNumber] ADD CONSTRAINT [FK_dbo.PhoneNumber_dbo.Employee_EmployeeModel_Id] FOREIGN KEY ([EmployeeModel_Id]) REFERENCES [dbo].[Employee] ([Id])
